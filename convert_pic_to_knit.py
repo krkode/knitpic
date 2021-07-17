3fe989e286eeca91e5a8f1cc116a05ee8ce20425
krkode <lambdaghini@gmail.com>
2021-07-17 22:46:56 +1000

    input pic outline, honestly i just want a lil green sqaure on github for the day

---
+++ convert_pic_to_knit.py
@@ -0,0 +1,22 @@
+#TODO: this should be javascript hey? why the hell am i doing this in python??!
+def request_pic():
+    pass
+
+def retrieve_dimensions():
+    pass
+
+def scale_pic():
+    pass
+
+def display_chart():
+    pass
+    
+
+def main():
+    request_pic()
+    retrieve_dimensions()
+    scale_pic()
+    display_chart()
+
+if __name__ == '__main__':
+    main()
